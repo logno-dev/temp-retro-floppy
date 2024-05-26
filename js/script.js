80d45759e8f9accde8c96be70838ab95bf2b8fe9
Logan Bunch <loganbunch@gmail.com>
2024-05-25 20:05:51 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const disk = document.getElementById("disk");
+const shadow = document.getElementById("shadow");
+
+disk.style.animation = "3s bounce infinite ease-in-out";
+shadow.style.animation = "3s shadow infinite ease-in-out";
